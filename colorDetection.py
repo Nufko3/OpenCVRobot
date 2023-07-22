90a3b27a5f9905aaf8afe1bdcfe8f6fe30cb6e4f
Nufko3 <kollarikmatko@gmail.com>
2023-07-22 16:39:44 +0200

    Image crop + emergency button

--- colorDetection.py
+++ colorDetection.py
@@ -14,8 +14,8 @@ u_h, u_s, u_v = 255, 255, 255 # 255, 255, 80
 l_b = np.array([l_h, l_s, l_v])
 u_b = np.array([u_h, u_s, u_v])
 
-powerButton = Button(26, pull_up = True, bounce_time = None)
-emergencyButton = None
+powerButton = Button(26, pull_up = False, bounce_time = None)
+emergencyButton = Button(4, pull_up = False, bounce_time = None)
 
 redLed = PWMLED(25)
 greenLed = PWMLED(24)
@@ -25,13 +25,13 @@ leftM = PWMLED(12)
 rightM = PWMLED(13)
 
 speed = 0.2
-P = 1
+P = 0.75
 centerPosition = 380
 
-exposure = 15
+exposure = 400#15
 minArea = 5000
 
-displayWindows = 1
+displayWindows = 0
 
 if system() == "Windows":
     linux = 0
@@ -40,11 +40,10 @@ else:
 
 print(linux)
 
-cv2.namedWindow("Camera", cv2.WINDOW_AUTOSIZE)
-
 if displayWindows:
+    cv2.namedWindow("Camera", cv2.WINDOW_AUTOSIZE)
     cv2.namedWindow("Value Editor")
-    cv2.createTrackbar("Color", "Value Editor", 230, 255, nothing)
+    cv2.createTrackbar("Color", "Value Editor", l_v, 255, nothing)
     cv2.createTrackbar("Exposure", "Value Editor", exposure, 400, nothing)
     cv2.createTrackbar("Min Area", "Value Editor", minArea, 30000, nothing)
 
@@ -62,6 +61,16 @@ capture.set(cv2.CAP_PROP_EXPOSURE, exposure)
 
 while True:
     #loopStartTime = time.time() * 1000
+    emergencyButtonPressed = not emergencyButton.is_pressed
+
+    if emergencyButtonPressed:
+        redLed.on()
+        greenLed.off()
+        blueLed.off()
+        leftM.off()
+        rightM.off()
+        time.sleep(0.1)
+        continue
 
     if displayWindows:
         l_v = cv2.getTrackbarPos("Color", "Value Editor")
@@ -74,6 +83,8 @@ while True:
 
     ret, frame = capture.read()
 
+    frame = frame[120:240, 60:580]
+
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(hsv, l_b, u_b)
@@ -85,13 +96,13 @@ while True:
         c = max(cnts, key = cv2.contourArea)
 
         if cv2.contourArea(c) > minArea:
-            cv2.drawContours(frame, [c], -1, (255, 0, 0), 3)
             M = cv2.moments(c)
             cx = int(M["m10"] / M["m00"])
             cy = int(M["m01"] / M["m00"])
             (x, y, w, h) = cv2.boundingRect(c)
 
             if displayWindows:
+                cv2.drawContours(frame, [c], -1, (255, 0, 0), 3)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
                 cv2.circle(frame, (cx, cy), 7, (255, 255, 255), -1)
                 cv2.putText(frame, f"{cx} {cy}", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
@@ -103,24 +114,10 @@ while True:
             errorP = P * error
             errorL = max(0, min(1, errorP))
             errorR = max(-1, min(0, errorP))
-            print(f"{error}\t{errorL}\t{errorR}\t{(1 - errorL) * speed}\t{(1 + errorR) * speed}")
+            #print(f"{error}\t{errorL}\t{errorR}\t{(1 - errorL) * speed}\t{(1 + errorR) * speed}")
 
             leftM.value = (1 - errorL) * speed
             rightM.value = (1 + errorR) * speed
-
-            '''
-            if pos < 50 and pos > -50:
-                print("F")
-                leftM.value = 0.2
-                rightM.value = 0.2
-            elif pos < -50:
-                print("L")
-                leftM.value = 0.2
-                rightM.value = 0.0
-            else:
-                print("R")
-                leftM.value = 0.0
-                rightM.value = 0.2'''
 
             redLed.off()
             greenLed.on()
@@ -149,7 +146,9 @@ while True:
         cv2.imshow("Mask", mask)
         cv2.imshow("Result", res)
 
-    if powerButton.is_pressed:
+    powerButtonPressed = not powerButton.is_pressed
+
+    if powerButtonPressed:
         redLed.on()
         greenLed.off()
         blueLed.off()
@@ -160,7 +159,9 @@ while True:
 
         time.sleep(5)
 
-        if powerButton.is_pressed:
+        powerButtonPressed = not powerButton.is_pressed
+
+        if powerButtonPressed:
             os.system("shutdown")
             break
         else:
